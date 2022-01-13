df267bff39b05be0ea55b445c98be79f6f16381e
LoL_Crunchy <81817993+lolcrunchy@users.noreply.github.com>
2022-01-13 16:02:55 +0100

    Delete src/main/java/dev/lolcrunchy/kitpvpcore directory

--- src/main/java/dev/lolcrunchy/kitpvpcore/Prefix.java
+++
@@ -1,8 +0,0 @@
-package dev.lolcrunchy.kitpvpcore;
-
-public class Prefix {
-
-    public static String prefix = "§8§l[ §3SnowPvP §8§l] §7";
-    public static String numPrefix = "§8§l[ §3SnowPvP §8§l] §c";
-
-}
